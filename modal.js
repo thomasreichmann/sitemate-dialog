fb546a7ff332b2279be4697c3493c529b98071e9
Thomas <thomasreichmann@hotmail.com>
2021-11-21 18:24:00 -0300

    modal visible atribute handling added

--- modal.js
+++ modal.js
@@ -3,13 +3,58 @@ template.innerHTML = `
 <style>
 <style>
 </style>
-<h1>The component loads!</h1>`;
+<dialog>
+  <menu>
+    <button class="cancel">Cancel</button>
+    <button class="confirm" value="default">Confirm</button>
+  </menu>
+</dialog>`;
 
 class Modal extends HTMLElement {
 	constructor() {
 		super();
 		this.attachShadow({ mode: 'open' });
 		this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+		let dialog = this.shadowRoot.querySelector('dialog');
+
+		// Check if modal is visible from the start
+		if (this.getAttribute('visible')) {
+			dialog.showModal();
+		}
+	}
+
+	connectedCallback() {
+		this.attachEventHandlers();
+	}
+
+	// Attach handlers to both buttons
+	attachEventHandlers() {
+		const cancelButton = this.shadowRoot.querySelector('.cancel');
+		cancelButton.addEventListener('click', e => {
+			this.removeAttribute('visible');
+		});
+		const confirmbutton = this.shadowRoot.querySelector('.confirm');
+		confirmbutton.addEventListener('click', e => {
+			this.removeAttribute('visible');
+		});
+	}
+
+	static get observedAttributes() {
+		return ['visible', 'title'];
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		if (name === 'title' && this.shadowRoot) {
+			this.shadowRoot.querySelector('.title').textContent = newValue;
+		}
+		if (name === 'visible' && this.shadowRoot) {
+			if (newValue === null) {
+				this.shadowRoot.querySelector('dialog').close();
+			} else {
+				this.shadowRoot.querySelector('dialog').showModal();
+			}
+		}
 	}
 }
 window.customElements.define('custom-modal', Modal);
